50c20a9a3574fb0d8773c8271a68f01d108df151
tom hikari <tomprescott1565@gmail.com>
2023-06-18 09:56:17 +0100

    feature/guardian

--- helpers/API/GuardianApi.php
+++ helpers/API/GuardianApi.php
@@ -9,25 +9,40 @@ use App\Helpers\Interfaces\FetchInterface;
 
 class GuardianApi implements ApiInterface
 {
+    public array $data = [];
+
+    public array $formatted = [];
 
     private $api_key;
 
     private FetchInterface $fetch;
 
-    public object $data;
+    private array $api_data_key;
+
 
 
     public function __construct(FetchInterface $fetch)
     {
         $this->api_key = env('GUARDIAN_API_KEY');
         $this->fetch = $fetch;
+        $this->api_data_key = [
+            "webTitle",
+            "webUrl",
+            "fields",
+            "pillarId",
+            "pillarName",
+            "sectionId",
+            "sectionName"
+        ];
     }
 
     public function headlines()
     {
         $url = "https://content.guardianapis.com/search?api-key=" . $this->api_key;
         $this->fetch->get($url);
-        $this->data = $this->fetch->response;
+        if ($this->fetch->response->status == "ok") {
+            $this->data = $this->fetch->response->response->results;
+        }
     }
 
     public function userFeed(string $type)
@@ -37,7 +52,9 @@ class GuardianApi implements ApiInterface
         $url .= "&api-key=" . $this->api_key;
 
         $this->fetch->get($url);
-        $this->data = $this->fetch->response;
+        if ($this->fetch->response->status == "ok") {
+            $this->data = $this->fetch->response->response->results;
+        }
     }
 
     public function search(string $search)
@@ -47,6 +64,29 @@ class GuardianApi implements ApiInterface
         $url .= "&api-key=" . $this->api_key;
 
         $this->fetch->get($url);
-        $this->data = $this->fetch->response;
+        if ($this->fetch->response->status == "ok") {
+            $this->data = $this->fetch->response->response->results;
+        }
+    }
+
+    public function format(array $format_fields)
+    {
+        $formatted = [];
+
+        foreach ($this->data as $index => $object) {
+            foreach ($object as $key => $value) {
+                if (!in_array($key, $this->api_data_key)) {
+                    continue;
+                }
+
+                if ($key == 'fields' && count($value) > 0) {
+                    $formatted[$index]['image'] = $value->thumbnail;
+                } else {
+                    $formatted[$index][$format_fields[array_search($key, $this->api_data_key)]] = $value;
+                }
+            }
+        }
+
+        $this->formatted = $formatted;
     }
 }
