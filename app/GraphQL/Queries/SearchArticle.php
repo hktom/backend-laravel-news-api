9fcd49801ed0f06376e4610e62edaa845e4a1c3f
tom hikari <tomprescott1565@gmail.com>
2023-06-18 11:31:54 +0100

    feature/search-articles

--- app/GraphQL/Queries/SearchArticle.php
+++ app/GraphQL/Queries/SearchArticle.php
@@ -4,8 +4,26 @@ namespace App\GraphQL\Queries;
 
 use App\Http\Controllers\ArticleController;
 
+use App\Helpers\API\NewsAPI;
+use App\Helpers\API\NewYorkTimeAPI;
+use App\Helpers\API\GuardianApi;
+use App\Helpers\Fetch;
+
+
 final class SearchArticle
 {
+    public array $article_schema = [
+        'title',
+        'description',
+        'content',
+        'image',
+        'url',
+        'publishedAt',
+        'source',
+        'author_name',
+        'category_name'
+    ];
+
     /**
      * @param  null  $_
      * @param  array{}  $args
@@ -15,8 +33,22 @@ final class SearchArticle
         if (!$args['search']) {
             return [];
         }
-        $articles = new ArticleController(env('NEWS_API_KEY'));
-        $articles->searchArticle($args['search']);
-        return $articles->articles;
+
+        $fetch = new Fetch();
+        $newsApi = new NewsAPI($fetch);
+        $newYorkTimeApi = new NewYorkTimeAPI($fetch);
+        $guardianApi = new GuardianApi($fetch);
+
+        $newsApi->search($args['search']);
+        $newYorkTimeApi->search($args['search']);
+        $guardianApi->search($args['search']);
+
+        $newsApi->format($this->article_schema);
+        $newYorkTimeApi->format($this->article_schema);
+        $guardianApi->format($this->article_schema);
+
+        $articles = array_merge($newsApi->formatted, $newYorkTimeApi->formatted, $guardianApi->formatted);
+
+        return $articles;
     }
 }
